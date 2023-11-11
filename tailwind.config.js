8ccdf6cf004175727e8e31b6fd22bdf5536431d1
pabloquirino <pabloquirino17@gmail.com>
2023-11-11 19:30:52 -0300

    em processo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'main': '1px 1px 5px 2px rgba(0, 0, 0, 0.74)'
+      },
+      colors: {
+        'gray-main': '#808080',
+        'condicao': 'rgb(83, 83, 83)',
+        'limite': 'rgb(59, 58, 58)'
+      },
+      fontFamily: {
+        'arial': 'Arial, Helvetica, sans-serif;'
+      },
+      spacing: {
+        '300px': '300px'
+      }
+    }
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [],
 }
 
