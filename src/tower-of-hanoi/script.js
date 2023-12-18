c517701fa3073ec40aeb1a8e44af8ac592ab2d6e
pabloquirino <pabloquirino17@gmail.com>
2023-12-18 12:00:46 -0300

    in process..

--- src/tower-of-hanoi/script.js
+++ src/tower-of-hanoi/script.js
@@ -1,6 +1,27 @@
-const rangeInput = document.getElementById('rangeInput')
-const rangeValue = document.getElementById('rangeValue')
+const rangeInput = document.querySelector('#rangeInput')
+const rangeValue = document.querySelector('#rangeValue')
 
 rangeInput.addEventListener('input', function () {
   rangeValue.textContent = rangeInput.value
 })
+
+let draggedItem
+
+function drag(event) {
+  event.dataTransfer.setData('text/plain', event.target.id)
+}
+
+function allowDrop(event) {
+  event.preventDefault()
+}
+
+function drop(event) {
+  event.preventDefault()
+
+  const data = event.dataTransfer.getData('text/plain')
+  const draggedElement = document.getElementById(data)
+
+  if (event.target.classList.contains('dropzone')) {
+    event.target.appendChild(draggedElement)
+  }
+}
