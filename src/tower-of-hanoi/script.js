c2bbfaba4a44a2f4233b09b93c46080a34bf0e68
pabloquirino <pabloquirino17@gmail.com>
2023-12-19 16:07:20 -0300

    finished

--- src/tower-of-hanoi/script.js
+++ src/tower-of-hanoi/script.js
@@ -2,40 +2,40 @@ const rangeInput = document.querySelector('#rangeInput')
 const rangeValue = document.querySelector('#rangeValue')
 
 rangeInput.addEventListener('input', () => {
-
+  
   const numElements = Number(rangeInput.value)
   rangeValue.textContent = rangeInput.value
-
+  
   let perfectMovement = window.document.querySelector('#perfectMovement')
 
   switch (numElements) {
 
     case 2:
-      perfectMovement.innerHTML = `| minimum amount of movements: 3`
-    break;
+      perfectMovement.innerHTML = `3`
+      break;
     
     case 3:
-      perfectMovement.innerHTML = `| minimum amount of movements: 7`
+      perfectMovement.innerHTML = `7`
     break;
 
     case 4:
-      perfectMovement.innerHTML = `| minimum amount of movements: 15`
-    break;
-
-    case 5:
-      perfectMovement.innerHTML = `| minimum amount of movements: 31`
-    break;
-    
-  }
-
-  //ocultar discos
-  const elements = document.querySelectorAll('[class^="bg-"]')
-  for (let i = 4; i >= numElements; i--) {
-    elements[i].style.display = 'none'
-  }
+      perfectMovement.innerHTML = `15`
+      break;
+      
+      case 5:
+        perfectMovement.innerHTML = `31`
+        break;
+        
+      }
+      
+      //ocultar discos
+      const elements = document.querySelectorAll('[class^="bg-"]')
+      for (let i = 4; i >= numElements; i--) {
+        elements[i].style.display = 'none'
+      }
   
-  //mostrar discos
-  for (let i = 0; i < numElements; i++) {
+      //mostrar discos
+      for (let i = 0; i < numElements; i++) {
     elements[i].style.display = 'block'
   }
 
@@ -55,24 +55,38 @@ function allowDrop(event) {
   event.preventDefault()
 }
 
+let moveCount = 0 //quantidade de movimentos, inicialmente 0
+
 function drop(event) {
   event.preventDefault()
-
+  
   const data = event.dataTransfer.getData('text/plain')
   const draggedElement = document.getElementById(data)
   const dropzone = event.target
-
+  
   if (dropzone.classList.contains('dropzone')) {
     const topDisk = dropzone.firstChild
-
+    
     dropzone.insertBefore(draggedElement, topDisk) //inserindo elemento arastável antes do primeiro filho existente
-
+    
+    //quantidade de movimentos
+    moveCount++
+    const moveCountElement = document.querySelector('#moveCount')
+    moveCountElement.textContent = `${moveCount}`
+    
     //verifica se o disco movido é maior que o disco no topo
     if (draggedElement.clientWidth > topDisk.clientWidth) {
       alert(`Movimento inválido!`)
       location.reload()
     } 
-   
-    
+
+    //parabeniza usuário
+    else if (moveCount == perfectMovement.textContent) {
+
+      setTimeout( () => {
+        alert(`Congratulations!`)
+      }, 100)
+
+    }
   }
 }
